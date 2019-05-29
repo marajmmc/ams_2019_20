b2d5e3870b574e3bd8c7542efacd920eb928cace
Md. Maraj Hossain Tarafder <marajmmc@gmail.com>
2019-05-29 13:55:52 +0600

    Barcode_helper barcode length 5

--- application/helpers/barcode_helper.php
+++ application/helpers/barcode_helper.php
@@ -6,7 +6,7 @@ class Barcode_helper
     // asset
     public static function get_barcode_asset($prefix, $increment_id)
     {
-        return $prefix.str_pad($increment_id,6,0,STR_PAD_LEFT);
+        return $prefix.str_pad($increment_id,5,0,STR_PAD_LEFT);
     }
 
 }
